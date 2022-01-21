36848e934fd06cf99c0b4883e0f40ffe8b17d20d
JonaRiosBrambila <95506962+JonaRiosBrambila@users.noreply.github.com>
2022-01-20 18:43:05 -0600

    JS-08 Introduction to Object Oriented Programming

--- app.js
+++ app.js
@@ -1,20 +1,63 @@
-const arreglo = ["Jonatan", "Rios", 34];
-
-console.log(arreglo[1]);
-
-let objeto = {
-    nombre: "Jonatan",
-    apellido: {
-        paterno: "Rios",
-        materno: "Brambila"
-    },
-    edad: 29,
-    nombreCompleto: function() {
-        return this.nombre + " " + this.apellido.paterno;
+/*
+class generation {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
 
     }
 
 }
-document.getElementById("boquita").innerHTML = objeto.nombreCompleto();
 
-//console.log(objeto.nombreCompleto());
+const persona1 = new persona("Jonatan", 1992);
+const persona2 = new persona("Jonatan", 2000);
+
+console.log(persona1);
+console.log(persona2);
+
+
+document.getElementById("ID").innerHTML = persona1.name;
+document.getElementById("ID").innerHTML = persona2.name;
+
+var objeto = {
+    name = "Jonatan",
+    year = 1992
+}
+
+var objeto2 = {
+    name = "David",
+    year = 1987
+}
+*/
+var apellido = prompt("Introduce el apellido del padre: ");
+var nombre1 = prompt("Introduce el nombre del primer hijo: ");
+var nombre2 = prompt("Introduce el nombre del segundo hijo: ");
+
+class padre {
+    constructor(apellido) {
+        this.apellidoPadre = apellido;
+    }
+}
+
+// var padre = {
+
+//   apellidoPadre:"parámetro del constructor"
+
+// }
+
+class hijo extends padre {
+    constructor(nombre, apellido) {
+        super(apellido);
+        this.nombreHijo = nombre;
+    }
+
+    registro() {
+        return (
+            "El nombre del hijo será: " + this.nombreHijo + " " + this.apellidoPadre);
+    }
+}
+
+let Hijo1 = new hijo(nombre1, apellido);
+let Hijo2 = new hijo(nombre2, apellido);
+
+document.getElementById("Id").innerHTML = Hijo1.registro();
+document.getElementById("Id2").innerHTML = Hijo2.registro();
